0f3cef3fd02374247bd110f719e5982cbb7379c1
ArturFox <arturfox1317@gmail.com>
2025-11-08 00:33:05 +0500

    Хз

--- src/services/grosbery.ts
+++
@@ -1,7 +0,0 @@
-import { Cart } from '@prisma/client';
-import { CartResponse, CreateCartItemValues } from '@/i-dont-no/dto';
-import { getRequest } from './instance';
-
-export const fetchCart = async (): Promise<CartResponse> => {
-  return await getRequest<CartResponse>('/cart');
-};
