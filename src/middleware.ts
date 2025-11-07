da2f36a621b064c3587c2a168952f4d825d6e53b
ArturFox <arturfox1317@gmail.com>
2025-11-08 03:08:50 +0500

    Добавил корс еще раз

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,8 @@ import type { NextRequest } from "next/server";
 export function middleware(req: NextRequest) {
   const res = NextResponse.next();
 
-  res.headers.set("Access-Control-Allow-Origin", "https://framework-t.ru");
+  //res.headers.set("Access-Control-Allow-Origin", "https://framework-t.ru");
+  res.headers.set("Access-Control-Allow-Origin", "*");
   res.headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
